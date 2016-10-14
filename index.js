3de0c204ca80c08cb57adad2667865936e49e66c
tjwudi <webmaster@leapoahead.com>
2016-10-13 23:04:22 -0700

    Memcached to store first interaction

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ if (process.env.NODE_ENV !== 'production') {
 
 var Bottr = require('bottr')
 var BottrApp = require('bottr-app')
+var memcachedClient = require('./lib/memcached/nice-client').create();
 
 var bot = new Bottr.Bot()
 bot.use(new BottrApp())
 bot.use(new Bottr.FacebookMessengerClient())
 
 bot.on('message_received', function (message, session) {
-    session.send('Hello! It\'s now ' + new Date());
+  memcachedClient.hadInteractionToday(message.user, function (err, hadInteractionToday) {
+    if (err) {
+      // TODO Exception handling.
+      throw err;
+    }
+
+    if (!hadInteractionToday) {
+      memcachedClient.setFirstInteractionToday(message.user, function () {
+        session.send('Hello! It\'s our first message today!');
+      });
+    }
+    else {
+      session.send('We already met today.');
+    }
+  });
 });
 
 bot.listen()
