0f4b2084f8c5e4423b5e566dc8067994d2e46f60
John Wu <webmaster@leapoahead.com>
2016-10-14 12:58:49 -0700

    refactor conversations

--- index.js
+++ index.js
@@ -6,27 +6,15 @@ if (process.env.NODE_ENV !== 'production') {
 var Bottr = require('bottr')
 var BottrApp = require('bottr-app')
 var memcachedClient = require('./lib/memcached/nice-client').create();
+var conversation = require('./lib/conversation');
 
 var bot = new Bottr.Bot()
 bot.use(new BottrApp())
 bot.use(new Bottr.FacebookMessengerClient())
 
 bot.on('message_received', function (message, session) {
-  memcachedClient.hadInteractionToday(message.user, function (err, hadInteractionToday) {
-    if (err) {
-      // TODO Exception handling.
-      throw err;
-    }
-
-    if (!hadInteractionToday) {
-      memcachedClient.setFirstInteractionToday(message.user, function () {
-        session.send('Hello! It\'s our first message today!');
-      });
-    }
-    else {
-      session.send('We already met today.');
-    }
-  });
+    // Facebook bot is currently the only source of message.
+    conversation.handleUserInteraction('facebook', message, session);
 });
 
 bot.listen()
