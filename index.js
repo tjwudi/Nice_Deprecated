4b19fc92df3dd46bd5568dabdd7cdf77ff039003
John Wu <webmaster@leapoahead.com>
2016-10-14 13:05:16 -0700

    ensure it's facebook traffic

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ if (process.env.NODE_ENV !== 'production') {
 
 var Bottr = require('bottr')
 var BottrApp = require('bottr-app')
-var memcachedClient = require('./lib/memcached/nice-client').create();
 var conversation = require('./lib/conversation');
 
 var bot = new Bottr.Bot()
@@ -13,8 +12,10 @@ bot.use(new BottrApp())
 bot.use(new Bottr.FacebookMessengerClient())
 
 bot.on('message_received', function (message, session) {
-    // Facebook bot is currently the only source of message.
-    conversation.handleUserInteraction('facebook', message, session);
+    if (session.client.constructor.name === 'FacebookMessengerClient') {
+        // Facebook bot is currently the only source of message.
+        conversation.handleUserInteraction('facebook', message, session);
+    }
 });
 
 bot.listen()
